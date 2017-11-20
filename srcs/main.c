619db527e226c86752bd660496488e82706acf80
Code141 <code141@iMac-de-Code141.local>
2017-11-20 21:04:15 +0100

    Minor changes

--- srcs/main.c
+++ srcs/main.c
@@ -2,17 +2,23 @@
 void	show_tetriminos(int tetriminos);
 /*----------------------------------------------------------------------------*/
 
+#include <unistd.h>
+
 int	main(int argc, char **argv)
 {
 	int	*tetriminos;
 
 	int i;
-
-	i = 0;
-	while (i < 10)	
-		show_tetriminos(i++);
-//	if (argc == 1)
-//		tetriminos = ft_parse_arg_to_binary(argv + 1);
-
+	if (argc == 2)
+	{
+		i = 0;
+		while (i < 5)	
+			show_tetriminos(i++);
+	}
+	else
+	{
+		write(2, "Usage: fillit target_file\n", 27);
+		return (1);
+	}
 	return (0);
 }
