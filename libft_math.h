1e2cd1ef10e0b7392da282781af9809c18527925
Sylvain BOILARD <sboilard@student.42.fr>
2017-11-23 20:07:58 +0100

    Norm.

--- libft_math.h
+++ libft_math.h
@@ -6,12 +6,12 @@
 /*   By: sboilard <sboilard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 17:39:10 by sboilard          #+#    #+#             */
-/*   Updated: 2017/11/23 17:39:48 by sboilard         ###   ########.fr       */
+/*   Updated: 2017/11/23 20:06:43 by sboilard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_MATH
-# define LIBFT_MATH
+#ifndef LIBFT_MATH_H
+# define LIBFT_MATH_H
 
 unsigned int	ft_sqrti(unsigned int nbr);
 
